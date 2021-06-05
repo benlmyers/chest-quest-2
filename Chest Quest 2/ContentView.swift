5435146b64e9085f4ab89052593edf956ded968d
Ben Myers <benben28405678@gmail.com>
2021-06-04 18:32:00 -0700

    Initial Commit

---
+++ Chest Quest 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chest Quest 2
+//
+//  Created by Ben Myers on 6/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
