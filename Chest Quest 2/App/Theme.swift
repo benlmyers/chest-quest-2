794e297e239dcd1205e856ae39bd0f356d66dd7d
Ben Myers <benben28405678@gmail.com>
2021-06-04 18:39:58 -0700

    🖌 Add Theme and colors

---
+++ Chest Quest 2/App/Theme.swift
@@ -0,0 +1,28 @@
+//
+//  Theme.swift
+//  Chest Quest 2
+//
+//  Created by Ben Myers on 6/4/21.
+//
+
+import SwiftUI
+
+struct Theme {
+  
+  static var z = Color("Background0")
+  static var y = Color("Background1")
+  static var x = Color("Background2")
+  
+  static var a = Color("Foreground0")
+  static var b = Color("Foreground1")
+  static var c = Color("Foreground2")
+  
+  static var sweat = Color("Health")
+  static var free = Color("Grow")
+  static var zone = Color("Grind")
+  
+  static var accent = Color("Accent")
+  
+  static var tabBar = Color("TabBar")
+  static var tabBarDivider = Color("TabBarDivider")
+}
